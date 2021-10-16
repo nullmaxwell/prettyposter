8b1d92bc23769c08e3418519930de64990df0c2f
nullmaxwell <nullmaxwell@protonmail.com>
2021-10-16 13:04:28 -0500

    Added test suites

---
+++ tests/test_prettyposter.py
@@ -0,0 +1,56 @@
+import pytest
+from src.prettyposter import Poster
+
+
+@pytest.fixture
+def test_instantiation():
+    """Verifies the instantiation of a poster object."""
+    test_poster = Poster((39.8283, 98.5795), (5, 5))
+    return test_poster
+
+
+def test_classAttributes(test_instantiation) -> Poster:
+    """
+    Tests the instantiation of the poster object
+    """
+    assert test_instantiation != None
+    assert test_instantiation.fig != None
+    assert test_instantiation.ax != None
+    assert test_instantiation.layers != None
+    return
+
+
+def test_reveal(test_instantiation):
+    """
+    Verify poster can be shown.
+    """
+    try:
+        test_instantiation.reveal()
+    except:
+        print(">>>> Failure to show poster.")
+        raise
+    return
+
+
+def test_export_default(test_instantiation):
+    """
+    Verify the exporting functionality of the poster object with the default file extension.
+    """
+    try:
+        test_instantiation.export("test_poster")
+    except:
+        print(">>>> Failure to export poster in default format.")
+        raise
+    return
+
+
+def test_export_png(test_instantiation):
+    """
+    Verify the exporting functionality of the poster object with a PNG extension.
+    """
+    try:
+        test_instantiation.export("test_poster", ".png")
+    except:
+        print(">>>> Failure to export poster in PNG format.")
+        raise
+    return
